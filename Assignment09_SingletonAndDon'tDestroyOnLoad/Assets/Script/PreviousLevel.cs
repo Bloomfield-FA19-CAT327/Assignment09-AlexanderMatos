28dc4d29aad6d6121a17aa8d1f82a93358dab2aa
AlexanderMatos1997 <47197201+AlexanderMatos1997@users.noreply.github.com>
2019-10-22 00:51:42 -0400

    Singleton and Don'tDestroyOnLoad is implemented

---
+++ Assignment09_SingletonAndDon'tDestroyOnLoad/Assets/Script/PreviousLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousLevel : MonoBehaviour
+{
+    void OnTriggerEnter()
+    {
+        SingletonGameController.Instance.PreviousLevel();
+    }
+}
---
+++ Assignment09_SingletonAndDon'tDestroyOnLoad/Assets/Script/SingletonGameController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SingletonGameController : MonoBehaviour
+{
+    Text hudText;
+
+    public int score;
+    public float timeInGame;
+    public int currentLevel;
+
+    private static SingletonGameController instance = null;
+    public static SingletonGameController Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<SingletonGameController>();
+                if(instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.name = "SimpleSingleton";
+                    instance = go.AddComponent<SingletonGameController>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        hudText = GameObject.Find("HUDText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string hudInfo = "";
+
+        timeInGame += Time.deltaTime;
+
+        hudInfo += "Level: " + (currentLevel + 1) + "\n";
+        hudInfo += "Score: " + score + "\n";
+        hudInfo += "Time: " + timeInGame.ToString("F2");
+
+        hudText.text = hudInfo;
+    }
+
+    public void NextLevel()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        LoadLevel();
+    }
+
+    public void PreviousLevel()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex - 1;
+        LoadLevel();
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
